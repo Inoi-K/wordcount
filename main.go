4a2bb5a9a3b732db4c111ae08aa6f86014a99a8d
Inoi-K <greatoat@gmail.com>
2022-07-09 18:41:31 +0300

    Solution w/ args

--- main.go
+++ main.go
@@ -1,26 +1,14 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
 	"strings"
 )
 
 func main() {
-	var in = bufio.NewReader(os.Stdin)
-	var out = bufio.NewWriter(os.Stdout)
-	defer out.Flush()
+	var line = strings.Join(os.Args[1:], " ")
 
-	var line string
-	line, _ = in.ReadString('\n')
-
-	var words = strings.Split(line, " ")
-	var wordCount int
-	for _, w := range words {
-		if len(w) > 0 {
-			wordCount++
-		}
-	}
-	fmt.Fprint(out, wordCount)
+	var words = strings.Fields(line)
+	fmt.Println(len(words))
 }
